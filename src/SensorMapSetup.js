27618c1f291bf8430701874085abd95f1c5b7b40
David Herbert <ndh114@newcastle.ac.uk>
2019-05-23 11:52:47 +0100

    Correct missing brackets in lambdas

--- src/SensorMapSetup.js
+++ src/SensorMapSetup.js
@@ -54,98 +54,102 @@ export const NEWCASTLE_CENTROID = [-1.6253, 54.9736];
  * Individual layer definitions
  * OpenStreetMap layer
  */
-export const OPENSTREETMAP = () => return(new TileLayer({
-	title: "OpenStreetMap",
-	type: "base",
-	visible: true,
-	source: new OSM()
-}));
+export const OPENSTREETMAP = () => {
+	return(new TileLayer({
+		title: "OpenStreetMap",
+		type: "base",
+		visible: true,
+		source: new OSM()
+	})
+});
 
 /**
  * LSOA layer
  */
-export const LSOA = () => return(new InteractiveVectorLayer(
-	{ /* Layer options */
-		title: "LSOAs", 
-		type: "overlay", 
-		cluster: false,
-		extent: NEWCASTLE_CENTRE_3857,
-		style: (feature) => {
-			return(new Style({
-				fill: new Fill({color: "rgba(215, 206, 199, 0.0)"}),
-				stroke: new Stroke({color: "rgba(118, 50, 63, 1)", width: 1})		
-			}));
-		}
-	},
-	{ /* Source options */
-		format: new GeoJSON(),
-		url: (extent) => {
-			return(`
-				http://ec2-52-207-74-207.compute-1.amazonaws.com:8080/geoserver/data_dot_gov/wfs?service=WFS&
-				version=2.0.0&request=GetFeature&typename=data_dot_gov:lsoa&
-				outputFormat=application/json&srsname=EPSG:3857&bbox=
-				` 
-				+ extent.join(",") + ",EPSG:3857");
+export const LSOA = () => {
+	return(new InteractiveVectorLayer(
+		{ /* Layer options */
+			title: "LSOAs", 
+			type: "overlay", 
+			cluster: false,
+			extent: NEWCASTLE_CENTRE_3857,
+			style: (feature) => {
+				return(new Style({
+					fill: new Fill({color: "rgba(215, 206, 199, 0.0)"}),
+					stroke: new Stroke({color: "rgba(118, 50, 63, 1)", width: 1})		
+				}));
+			}
 		},
-		strategy: bboxStrategy		
-	},
-	{ /* Hover options */
-		style: (feature) => {
-			return(new Style({
-				stroke: new Stroke({color: "rgba(192, 159, 128, 1)", width: 1}),
-				fill: new Fill({color: "rgba(192, 159, 128, 0.4)"}),
-				text: new Text({
-					font: "12px consolas",
-					text: feature.get("lsoa11nm"),
-					overflow: true,
-					stroke: new Stroke({color: "rgba(118, 50, 63, 1)"}),
-					fill: new Fill({color: "rgba(118, 50, 63, 1)"})
-				})
-			}));
-		}
-	}
-));
+		{ /* Source options */
+			format: new GeoJSON(),
+			url: (extent) => {
+				return(`
+					http://ec2-52-207-74-207.compute-1.amazonaws.com:8080/geoserver/data_dot_gov/wfs?service=WFS&
+					version=2.0.0&request=GetFeature&typename=data_dot_gov:lsoa&
+					outputFormat=application/json&srsname=EPSG:3857&bbox=
+					` 
+					+ extent.join(",") + ",EPSG:3857");
+			},
+			strategy: bboxStrategy		
+		},
+		{ /* Hover options */
+			style: (feature) => {
+				return(new Style({
+					stroke: new Stroke({color: "rgba(192, 159, 128, 1)", width: 1}),
+					fill: new Fill({color: "rgba(192, 159, 128, 0.4)"}),
+					text: new Text({
+						font: "12px consolas",
+						text: feature.get("lsoa11nm"),
+						overflow: true,
+						stroke: new Stroke({color: "rgba(118, 50, 63, 1)"}),
+						fill: new Fill({color: "rgba(118, 50, 63, 1)"})
+					})
+				}));
+			}
+		})
+});
 
 /**
  * Sensor layer
  */
-export const SENSORS = () => return(new InteractiveVectorLayer(
-	{
-		title: "Sensor locations",
-		type: "overlay",
-		cluster: true,
-		visible: true,		
-		style: (feature) => {
-			let	clusterFeats = feature.get("features");
-			let size = clusterFeats ? clusterFeats.length : 1;
-			style = new Style({
-				image: new CircleStyle({
-					radius: 5,
-					fill: new Fill({color: "white"}),
-					stroke: new Stroke({color: "red", width: 2})
-				})
-			});
-			if (size > 1) {
-				style.setText(new Text({
-					text: size.toString(),
-					fill: new Fill({color: "#ff0000"})
-				}));
+export const SENSORS = () => {
+	return(new InteractiveVectorLayer(
+		{
+			title: "Sensor locations",
+			type: "overlay",
+			cluster: true,
+			visible: true,		
+			style: (feature) => {
+				let	clusterFeats = feature.get("features");
+				let size = clusterFeats ? clusterFeats.length : 1;
+				style = new Style({
+					image: new CircleStyle({
+						radius: 5,
+						fill: new Fill({color: "white"}),
+						stroke: new Stroke({color: "red", width: 2})
+					})
+				});
+				if (size > 1) {
+					style.setText(new Text({
+						text: size.toString(),
+						fill: new Fill({color: "#ff0000"})
+					}));
+				}
+				return(style);
 			}
-			return(style);
-		}
-	},
-	{}, {},
-	{
-		ordering: ["Name", "Latitude", "Longitude", "Elevation", "Height", "Broker", "Third party"],
-		translation: {
-			"Broker": "Broker Name", 
-			"Elevation": "Ground Height Above Sea Level",
-			"Latitude": "Sensor Centroid Latitude", 
-			"Longitude": "Sensor Centroid Longitude", 
-			"Height": "Sensor Height Above Ground",
-			"Name": "Sensor Name",
-			"Third party": "Third Party"
 		},
-		nameattr: "Name"			
-	}
-));
+		{}, {},
+		{
+			ordering: ["Name", "Latitude", "Longitude", "Elevation", "Height", "Broker", "Third party"],
+			translation: {
+				"Broker": "Broker Name", 
+				"Elevation": "Ground Height Above Sea Level",
+				"Latitude": "Sensor Centroid Latitude", 
+				"Longitude": "Sensor Centroid Longitude", 
+				"Height": "Sensor Height Above Ground",
+				"Name": "Sensor Name",
+				"Third party": "Third Party"
+			},
+			nameattr: "Name"			
+		})
+});
